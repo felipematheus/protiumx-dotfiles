4910fc16a67224fe0e666c1de4af02423ec2377f
Felipe Rosa <felipe.mr89@gmail.com>
2024-11-11 21:29:07 -0300

    feat: add config files

---
+++ config/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    
+    # Commands to run in interactive sessions can go here
+end
+eval "$(/opt/homebrew/bin/brew shellenv)"
+starship init fish | source
+enable_transience
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
